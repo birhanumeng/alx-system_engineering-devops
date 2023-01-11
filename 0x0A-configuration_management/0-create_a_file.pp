4f347b49439d9a670bd7e8379c165e49d51b4919
birhanumeng <eyerusalemmengiste@gmail.com>
2023-01-10 23:48:41 -0800

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp directory
 file { '/tmp/school':
-   ensure  => 'present',
-   mode => '0744',
-   owner => 'www-data',
-   group => 'www-data',
-   content => 'I love Puppet',
+  ensure  => 'present',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
