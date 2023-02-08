8b0eab0fa425c4379a88a220a22641e00467d558
= <=>
2023-02-08 14:23:21 +0000

    list employee's to do

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "user{}".format(argv[1])).json()
-    tasks = requests.get(url + "todo", params={"userId": argv[1]}).json()
+    user = requests.get(url + "users{}".format(argv[1])).json()
+    tasks = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
     completed = [t.get("title") for t in tasks if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
