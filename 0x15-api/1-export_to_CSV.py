9326161f293a993c6b389c955d9d1ece2e04dbde
= <=>
2023-02-08 19:08:05 +0000

    csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""returns information about employee's TODO list progress"""
+""" export data in the CSV format"""
 import csv
 import requests
 from sys import argv
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(argv[1])).json()
     tasks = requests.get(url + "todos", params={"userId": argv[1]}).json()
-    userId = argv[1]
     username = user.get("username")
 
     with open("{}.csv".format(argv[1]), "w", newline="") as csvfile:
