730ed69635b94f4dfd478fc2eb4fa22b20a5085b
= <=>
2023-02-28 18:06:36 +0000

    print frst 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 
-10 hot posts listed for a given subreddit.
-"""
+"""Queries the Reddit API and prints the titles of the first 10"""
 import requests
 
 
