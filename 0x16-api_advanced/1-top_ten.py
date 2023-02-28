a6cbe1a052fb4fd47d729b2d5fdf360b0c463ff6
= <=>
2023-02-28 17:41:50 +0000

    print first 10 hot pots

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first 
+10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts."""
+
+    req = requests.get("https://reddit.com/r/{}.json?sort=hot&limit=10".
+                     format(subreddit), headers={"User-Agent": "custom"})
+
+    if (req.status_code == 200):
+        for item in r.json().get("data").get("children"):
+            print(item.get("data").get("title"))
+    else:
+        print("None")
