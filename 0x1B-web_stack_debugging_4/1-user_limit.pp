e690ecc7ad0e44d05e975e92fb8f10f29daff633
= <=>
2023-03-11 14:44:53 +0000

    configure for holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+#configure to enable login holberton user and open a file without error message.
+
+#configure hard limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/bin/'
+}
+
+#configure soft limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/bin/'
+}
